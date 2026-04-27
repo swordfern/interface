842887dbc2d724155c3fc82cb64ead4f8f19bd96
marlon-erler <viridian035@gmail.com>
2026-04-27 16:23:48 +0000

    restrict text components

--- textComponents.ts
+++ textComponents.ts
@@ -1,6 +1,22 @@
 import { Component } from "./component";
+import { Renderer } from "./renderer";
+
+export enum TextComponentTypes {
+    Paragraph = "p",
+    Span = "span",
+    Label = "label",
+
+    PrimaryHeadline = "h1",
+    SecondaryHeadline = "h2",
+}
 
 export class TextComponent extends Component {
+    // init
+    constructor(renderer: Renderer, type: TextComponentTypes) {
+        super(renderer, type);
+    }
+
+    // specific
     private _textContent: string = "";
     get textContent(): string {
         return this._textContent;
